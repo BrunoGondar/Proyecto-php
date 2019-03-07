51ccf183f41beedf6613912c01d3ca9254b5c91b
aleixo <aleixof.r@gmail.com>
2019-03-08 00:00:20 +0100

    Create conexion_bd.php

---
+++ core/conexion_bd.php
@@ -0,0 +1,11 @@
+<?php
+
+    define("SERVER", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("DB", "proyecto");
+
+
+    $mysqli = new mysqli(SERVER, USER, PASS, DB); 
+
+?>
