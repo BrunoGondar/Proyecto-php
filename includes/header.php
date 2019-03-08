0ade214af5aef0a414af014dc707c31ff854c41b
aleixo <aleixof.r@gmail.com>
2019-03-08 18:23:42 +0100

    xd

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<nav class='navegador'>
+            <div class='logo'><img src='./imagenes/logo.png'></div>
+            <div class='enlaces'>
+                <div>Inicio</div>
+                <div>Catálogo</div>
+                <div>Proveedores</div>
+            </div>
+        </nav>";
+?>
