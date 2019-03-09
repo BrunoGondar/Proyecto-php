954759b8b42b6fe91f63cffc20ac4e6dd98708d9
aleixo <aleixof.r@gmail.com>
2019-03-09 21:16:42 +0100

    Header de la pagina

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,14 @@
 <?php
     echo "<nav class='navegador'>
-            <div class='logo'><img src='./imagenes/logo.png'></div>
-            <div class='enlaces'>
-                <div>Inicio</div>
-                <div>Catálogo</div>
-                <div>Proveedores</div>
+            <div class='logo-wrapper'><img src='./imagenes/logo.png'></div>
+            <div class='enlaces-wrapper'>
+                <a>Inicio</a>
+                <a>Catálogo</a>
+                <a>Proveedores</a>
+            </div>
+            <div class='log-wrapper'>
+                <a href='#'>Sign Up</a>
+                <a href='#'>Log In</a>
             </div>
         </nav>";
 ?>
