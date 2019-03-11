947fb2b08a8621e5e2d51652210bac02b2f12da1
Aleixo Fernández Rodríguez <aleixofernandez10@gmail.com>
2019-03-11 13:47:24 +0100

    pie de pagina

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<?php
+// file: /includes/footer.php
+
+    /**
+     * Fichero en el que se encuentra el pie de pagina
+    */
+echo "<footer class='foo'>
+        <div class='top-foo'>        
+            <div class='logo'></div>
+            <div class='ofertas'>
+                <label for='ofertas'>¿Quieres Recibir todas las ofertas que hacemos?</label>
+                <input type='email' placeholder='correo@gmail.com' name='email-sub' value=''>
+            <div>
+        </div>
+        <div class='botton-foo'>
+            <a href='#'>Quienes somos</a>
+            <a href='#'>Contacto</a>
+            <a href='#'>Aviso legal</a>
+            <a href='#'>Ofertas de empleo</a>
+        </div>
+    </footer>";
+
+?>
