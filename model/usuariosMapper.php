a04f16a6a4732dbe40d98aa5594f4b0a5ac613bb
Aleixo Fernández Rodríguez <aleixofernandez10@gmail.com>
2019-03-11 13:46:23 +0100

    no nec

--- model/usuariosMapper.php
+++ model/usuariosMapper.php
@@ -1,6 +1,7 @@
 <?php
 // file: model/UsuarioMapper.php 
-require_once (__DIR__ . "/../core/conexion_bd.php");
+
+require_once (__DIR__ . "/../core/PDOConnection.php");
 
 /**
  * Clase UsuarioMapper
@@ -14,7 +15,7 @@ class UsuarioMapper
     /**
     * Referencia a la conexion a la base de datos
     *
-    * @var mysqli
+    * @var PDO
     */
 
     private $db;
@@ -34,11 +35,11 @@ class UsuarioMapper
 
     public function guardar($user)
     {
-        $sentencia = $this->db->prepare("INSERT INTO usuarios(nk_usuario, pass, tipo, nombre, apellido1, apellido2, email)
+        $sentencia = $this->db->prepare("INSERT INTO usuarios(login, pass, tipo, nombre, apellido1, apellido2, email)
                                                 VALUES (?,?,?,?,?,?,?)");
 
         $sentencia->execute(array(
-            $user->getNick(),
+            $user->getLogin(),
             $user->getPass(),
             $user->getTipo(),
             $user->getNombre(),
@@ -68,15 +69,15 @@ class UsuarioMapper
     /**
      * Editar a un usuario
      * 
-     * @param $id, $nick, $pass, $tipo, $nombre, $apellido_1, $apellido_2, $email
+     * @param $id, $login, $pass, $tipo, $nombre, $apellido_1, $apellido_2, $email
      * @return void
      */
 
     public function editar($id, $nick, $pass, $tipo, $nombre, $apellido_1, $apellido_2, $email)
     {
-        $sentencia = $this->db->prepare("UPDATE usuarios SET nk_usuario = ?, pass = ?, tipo = ?, nombre = ?, apellido1 = ?, apellido2 = ?, email= ? WHERE id = ?");
+        $sentencia = $this->db->prepare("UPDATE usuarios SET login = ?, pass = ?, tipo = ?, nombre = ?, apellido1 = ?, apellido2 = ?, email= ? WHERE id = ?");
         $sentencia->execute(array(
-            $nick,
+            $login,
             $pass,
             $tipo,
             $nombre,
@@ -86,13 +87,86 @@ class UsuarioMapper
             $id
         ));
     }
+
+    /**
+     * Comprueba si existe un usuario 
+     *
+     * @param $id
+     * @return boolean
+     */
+    public function existe($id)
+    {
+        $sentencia = $this->db->prepare("SELECT count(id) FROM usuarios where id=?");
+        $sentencia->execute(array(
+            $id
+        ));
+        
+        if ($sentencia->fetchColumn() > 0) {
+            return true;
+        }
+    }
+
+    /**
+     * Comprueba si el usuario es valido
+     *
+     * @param $id, $pass
+     * @return boolean
+     */
+    public function valido($id, $pass)
+    {
+        $sentencia = $this->db->prepare("SELECT count(id) FROM usuarios where login=? and pass=?");
+        $sentencia->execute(array(
+            $login,
+            $pass
+        ));
+        
+        if ($sentencia->fetchColumn() > 0) {
+            return true;
+        }
+    }
+
+    /**
+     * Devuelve los datos de un usuario 
+     *
+     * @param $login
+     * @return Usuario
+     */
+    public function getDatos($login)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM usuarios where login=?");
+        $stmt->execute(array(
+            $login
+        ));
+        
+        $toret_db = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+        
+        $user = null;
+        
+        foreach ($toret_db as $datos) {
+            $user = new Usuario($datos['id'], $datos['login'], $datos['tipo'], $datos['nombre'], $datos['apellido1'], $datos['apellido2'], $datos['email']);
+        }
+        
+        return $user;
+    }
+
+    /**
+     * Devuelve todos los usuarios 
+     *
+     * 
+     * @return mixed Usuario
+     */
+    public function getUsuarios()
+    {
+        $sentencia = $this->db->query("SELECT * FROM usuarios");
+        $toret_db = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+        
+        $users = array();
+        
+        foreach ($toret_db as $user) {
+            array_push($users, new Usuario($user['id'], $user['login'], $user['tipo'], $user['nombre'], $user['apellido1'], $user['apellido2'], $user['email']));
+        }
+        
+        return $users;
+    }
 }
-
-
-
-
-
-
-
-
 ?>
---
+++ view/about/mostrar.php
@@ -0,0 +1,30 @@
+<?php
+// file: /view/about/mostrar.php
+
+    /**
+     * Vista que mostrará informacion sobre la tienda
+    */
+    
+    require_once (__DIR__ . "/../../includes/header.php");
+?>
+
+    <div class='wrapper-contenido'>
+        <div class='wrapper-imagen-1'>
+            <div class="imagen"></div>
+            <div class="explicacion"></div>
+        </div>
+        <div class='wrapper-imagen-2'>
+            <div class="imagen"></div>
+            <div class="explicacion"></div>
+        </div>
+        <div class='wrapper-imagen-3'>
+            <div class="imagen"></div>
+            <div class="explicacion"></div>
+        </div>
+    </div>
+
+<?php
+
+    require_once (__DIR__ . "/../../includes/footer.php");
+
+?>
