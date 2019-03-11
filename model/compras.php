91e3235aacb4ba783f9ebda2338ebbf9ce0ff6b0
Aleixo Fernández Rodríguez <aleixofernandez10@gmail.com>
2019-03-11 12:10:50 +0100

    Objeto Compra

---
+++ model/compras.php
@@ -0,0 +1,112 @@
+<?php
+
+    class Compra
+    {
+        /**
+        * Id de la compra
+        * @var int
+        */
+
+        private $id;
+
+        /**
+        * Codigo del usuario que realiza la compra
+        * @var int
+        */
+
+        private $cod_usuario;
+
+        /**
+        * Precio de la compra
+        * @var int
+        */
+        
+        private $precio;
+
+        /**
+        * fecha en la compra
+        * @var string
+        */
+
+        private $fecha;
+
+        public function __construct($id, $cod_usuario, $precio, $fecha)
+        {
+            $this->id          = $id;
+            $this->cod_usuario = $cod_usuario;
+            $this->precio      = $precio;
+            $this->fecha       = $fecha;
+        }
+        
+        /**
+        * Devuelve el Id de la compra
+        * @return int
+        */
+
+        public function getID()
+        {
+            return  $this->id;
+        }
+
+        /**
+        * Devuelve el codigo del usuario que realiza la compra
+        * @return int
+        */
+
+        public function getCod_usuario()
+        {
+            return $this->cod_usuario;
+        }
+
+        /**
+        * Cambia el valor del codigo del Usuario que realiza la compra
+        * @return void
+        */
+
+        public function setCod_usuario($cod_usuario)
+        {
+            $this->cod_usuario = $cod_usuario;
+        }
+
+
+        /**
+        * Devuelve el precio de la compra
+        * @return int
+        */
+
+        public function getPrecio()
+        {
+            return $this->precio;
+        }
+
+        /**
+        * Cambia el precio de la compra
+        * @return void
+        */
+
+        public function setPrecio($precio)
+        {
+            $this->precio = $precio;
+        }
+
+        /**
+        * Devuelve la Fecha de la compra
+        * @return string
+        */
+
+        public function getFecha()
+        {
+            return $this->fecha;
+        }
+
+        /**
+        * Cambia el valor de la fecha de la compra 
+        * @return void
+        */
+
+        public function setFecha($fecha)
+        {
+            $this->fecha = $fecha;
+        }
+    }
+?>
