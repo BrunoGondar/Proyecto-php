1f6adc672a9c71f086e16c273abfc7e0017ce0a9
Aleixo Fernández Rodríguez <aleixofernandez10@gmail.com>
2019-03-11 13:46:57 +0100

    validacion de excepciones

---
+++ core/ValidationException.php
@@ -0,0 +1,40 @@
+<?php
+// file: core/ValidationException.php
+
+    /**
+     * Clase ValidationException
+     *
+     * Una excepción simple que incluye una serie de errores.
+     * Útil para la validación de formularios.
+     * La matriz de errores contiene errores de validación, normalmente
+     * indexado por formulario denominado parámetros.
+     *
+     */
+
+    class ValidationException extends Exception
+    {
+
+        /**
+         * Array de errores
+         *
+         * @var mixed
+         */
+        private $errors = array();
+
+        public function __construct(array $errors, $msg = NULL)
+        {
+            parent::__construct($msg);
+            $this->errors = $errors;
+        }
+
+        /**
+         * Obtiene los errores de validación.
+         *
+         * @return mixed Los errores de validación.
+         */
+        public function getErrors()
+        {
+            return $this->errors;
+        }
+    }
+?>
