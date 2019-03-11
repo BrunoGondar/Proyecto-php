4ce442446d1a7f33d833bff175772566a4651a21
Aleixo Fernández Rodríguez <aleixofernandez10@gmail.com>
2019-03-11 12:16:40 +0100

    Objeto Proveedor

---
+++ model/proveedores.php
@@ -0,0 +1,85 @@
+<?php
+
+    class Proveedor
+    {
+        /**
+        * Id del Proveedor
+        * @var int
+        */
+
+        private $id;
+
+        /**
+        * nombre del Proveedor
+        * @var string
+        */
+
+        private $nombre;
+
+        /**
+        * URL de la imagen del Proveedor
+        * @var string
+        */
+
+        private $imagen;
+              
+        public function __construct($id = NULL, $nombre = NULL, $imagen = NULL)
+        {
+            $this->id = $id;
+            $this->nombre = $nombre;
+            $this->imagen = $imagen;
+        }
+
+        /**
+        * Devuelve el Id del Proveedor
+        * @return int
+        */
+
+        public function getId()
+        {
+            return $this->id;
+        }
+
+
+        /**
+        * Devuelve el nombre del Proveedor
+        * @return string
+        */
+
+        public function getnombre()
+        {
+            return $this->nombre;
+        }
+
+        /**
+        * Cambia el valor del nombre del Proveedor
+        * @return void
+        */
+
+        public function setnombre($nombre)
+        {
+            $this->nombre = $nombre;
+        }
+
+
+        /**
+        * Devuelve la URL de la imagen del Proveedor
+        * @return string
+        */
+
+        public function getimagen()
+        {
+            return $this->imagen;
+        }
+
+        /**
+        * Cambia el valor de la URL de la imagen del Proveedor
+        * @return void
+        */
+        
+        public function setimagen($imagen)
+        {
+            $this->imagen = $imagen;
+        }
+    }
+?>
